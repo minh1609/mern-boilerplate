7ba68734b923388e7ccd64d13fb8719a6ef78518
minh <pointman449@gmail.com>
2019-07-11 16:17:01 -0700

    upload

---
+++ README.md
@@ -0,0 +1,4 @@
+Want to collect your client’s opinion about your product, service ? Give this app a list of your client’s
+email, It will help you to collect all the feedback
+
+This is a part of Stephen Grider coure on Udemy
---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID:
+        "408509518857-saojal6407pmiscpoarik8napgl4k29p.apps.googleusercontent.com",
+    googleClientSecret: "9ijioI21iyyyvoi0yRT4S_Td",
+    mongoDB:
+        "mongodb+srv://ADMIN-MINH:mM160997%40@cluster0-gjhw3.mongodb.net/main",
+    cookieKey: "mmmmaswdmwdon",
+    stripeSecretKey: "sk_test_ojDFDq4nZYBuPVE7R23i9MVM00gz7IfACp",
+    stripePublishableKey: "pk_test_38tD5VgkInaEO0SUyYI227Cg00t6BE0QEO",
+    sendGridKey:
+        "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"
+};
